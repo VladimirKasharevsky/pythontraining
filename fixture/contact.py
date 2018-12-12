c332d176dd8edd2205614aa831dd0969191e3b55
Vladimir Kasharevskiy <kashar015@rambler.ru>
2018-12-12 06:00:13 +0300

    Выделен класс для работы с контактами

---
+++ fixture/contact.py
@@ -0,0 +1,34 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+
+    def create(self, contact):
+        # Fill contact info
+        wd = self.app.wd
+        wd.find_element_by_xpath("//div[@id='content']/form/label").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company_name)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.mail)
+        # Submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def open_add_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
---
+++ test/test_add_contact.py
@@ -0,0 +1,17 @@
+import pytest
+from model.contact import Contact
+from fixture.application import  Application
+
+@pytest.fixture
+def app(request):
+        fixture = Application()
+        request.addfinalizer(fixture.destroy)
+        return fixture
+
+
+def test_add_contacts(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.open_add_contact_page()
+    app.contact.create(Contact(name="pyt", last_name="pyt", company_name="pyt", address="pyt", mail="pyt@pyt.py"))
+    app.session.logout()
+
