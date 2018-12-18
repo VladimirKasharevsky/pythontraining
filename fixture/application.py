7da0d448fa404f39b7e9370656ecd95e24cd90b3
Vladimir Kasharevskiy <kashar015@rambler.ru>
2018-12-18 05:51:10 +0300

    Доработана фикстура

--- fixture/application.py
+++ fixture/application.py
@@ -18,7 +18,7 @@ class Application:
             self.wd.current_url
             return True
         except:
-            return  False
+            return False
 
     def open_start_page(self):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,3 +5,9 @@ def test_add_group(app):
     app.group.open_group_page()
     app.group.init_group_creation()
     app.group.create(Group(name="pyt", header="pyt", footer="pyt"))
+   # app.session.logout()
+
+def test_add_empty_group(app):
+    app.group.open_group_page()
+    app.group.init_group_creation()
+    app.group.create(Group(name="", header="", footer=""))
