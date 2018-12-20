8955d2c622a89fcff9937b2e77f09fb9cfdb7861
Vladimir Kasharevskiy <kashar015@rambler.ru>
2018-12-20 03:40:00 +0300

    Доооформлены тесты

--- fixture/application.py
+++ fixture/application.py
@@ -8,12 +8,13 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
     def is_valid(self):
+        wd = self.app.wd
         try:
             self.wd.current_url
             return True
