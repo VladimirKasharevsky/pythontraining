8cafafc927b44608bf55ab43e6cd45dcd072431e
Vladimir Kasharevskiy <kashar015@rambler.ru>
2018-12-20 04:35:44 +0300

    Реализована возможность редактирования разных параметров в тестах модификации групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,3 +4,11 @@ from model.group import Group
 def test_edit_firs_group(app):
     app.group.select_edited_group()
     app.group.edit(Group(name="pytedit", header="pytedit", footer="pytedit"))
+
+def test_edit_first_grop_name(app):
+    app.group.select_edited_group()
+    app.group.modify_first_group(Group(name="New group"))
+
+def test_edit_first_grop_header(app):
+    app.group.select_edited_group()
+    app.group.modify_first_group(Group(header="New header"))
