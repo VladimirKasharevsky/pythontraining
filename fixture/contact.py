8583dff76b13c100977a2424614fb676ebe1c289
Vladimir Kasharevskiy <kashar015@rambler.ru>
2018-12-21 03:20:10 +0300

    Реализована проверка наличия контактов перед их редактированием

--- fixture/contact.py
+++ fixture/contact.py
@@ -77,7 +77,7 @@ class ContactHelper:
 
     def count(self):
         wd = self.app.wd
-        self.open_add_contact_page()
+        #self.open_add_contact_page()
         return len(wd.find_elements_by_name("selected[]"))
 
 
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,17 +1,9 @@
 from model.contact import Contact
 
 
-def test_edit_contacts(app):
-    if app.contact.count()==0:
-        app.contact.open_add_contact_page()
-        app.contact.create(Contact(name="py", last_name="py", company_name="py", address="py", mail="py@py.py"))
-    app.contact.edit_first_contact()
-    app.contact.edit(Contact(name="pytedit", last_name="pytedit", company_name="pytedit", address="pytedit", mail="pytedit@pytedit.py"))
-
-
 def test_edit_first_contacts(app):
     if app.contact.count() == 0:
        app.contact.open_add_contact_page()
        app.contact.create(Contact(name="py", last_name="py", company_name="py", address="py", mail="py@py.py"))
     app.contact.edit_first_contact()
-    app.contact.edit(Contact(name="1"))
+    app.contact.edit(Contact(name="2"))
